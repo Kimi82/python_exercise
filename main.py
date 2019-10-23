d52c89014da15c9647d08c65630675be2f012c5c
Kimi82 <cyrekamil@gmail.com>
2019-10-23 10:01:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ clientSecret = '39fcabe84a47bf2351b2fb4e77aa07a05aa0c51b'
 clientID = '418c0925f25a9b6e'
 apiToken = '65001d59010b6b7edfc9bf1fee0535b82b74b1d2'
 companyDomain = 'edward-sandbox-9deccb'
-#url = 'https://' + companyDomain + '.pipedrive.com/v1/deals?api_token=' + apiToken
-rly = 'https://edward-sandbox-9deccb.pipedrive.com/v1/deals?api_token=65001d59010b6b7edfc9bf1fee0535b82b74b1d2'
-
-client = Client(clientID, clientSecret)
-
-url = client.authorization_url(rly)
-token = client.exchange_code(rly, 'CODE')
-client.set_access_token('ACCESS_TOKEN')
-token = client.refresh_token('REFRESH_TOKEN')
 
 
 def create_organization():
+    url = 'https://companydomain.pipedrive.com/v1/organizations?api_token=' + apiToken
+    client = Client(domain=url)
+    client.set_api_token(apiToken)
+
     data = {
-        "title": 'NIE',
+        "name": 'My Organization',
     }
     response = client.organizations.create_organization(data)
 
 def add_users(how):
+    
+
     count = 0
     while count < how:
         data = {
-         'title': 'Employee'+ str(count)
+         'name': 'Employee'+ str(count)
+         'org_id' : count
         }
-        response = client.persons.create_person(data)
+        personID = count
+        
+        client.persons.create_person(data)
         count+=1
 
 create_organization()
-#add_users(2)
+add_users(2)
