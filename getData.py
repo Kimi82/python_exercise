07fc89388e832efd9cae98978e0a010e73aa3cd4
Kimi82 <cyrekamil@gmail.com>
2019-12-28 23:33:09 +0100

    Create getData.py

---
+++ getData.py
@@ -0,0 +1,9 @@
+orgName = input("Enter name of your organization: ")
+homManyEmployes= input("Enter how many employees you have: ")
+homManyEmployes = int(homManyEmployes)
+
+employees = []
+
+for x in range(homManyEmployes):
+    employees.append(input("Enter name of your employe: "))
+
